57f4b006948681282f068dc8ac336d620251d744
Lisa M Schachtschneider <islisamarie@gmail.com>
2014-08-11 14:55:12 +0200

    styling fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 get '/about' => 'pages#about'
-
+resources :pages
   resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
