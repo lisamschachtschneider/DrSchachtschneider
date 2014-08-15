7dc916c2abbbb21fd9a600acc191b2ba5eb03f85
Lisa M Schachtschneider <islisamarie@gmail.com>
2014-08-15 13:29:06 +0200

    paperclip gem to attach images to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811101905) do
+ActiveRecord::Schema.define(version: 20140815102843) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
