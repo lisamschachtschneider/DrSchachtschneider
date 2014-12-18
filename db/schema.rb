f02a1bcb5053c60c785aa81144dc8a82600570c5
Lisa M Schachtschneider <islisamarie@gmail.com>
2014-12-18 13:32:44 +0100

    adding projects to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218121504) do
+ActiveRecord::Schema.define(version: 20141218122945) do
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "publications", force: true do |t|
     t.string   "title"
