35fc12800fdd958613f02c35521b53bd43ea2059
Lisa M Schachtschneider <islisamarie@gmail.com>
2014-12-18 13:15:34 +0100

    adding tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218121300) do
+ActiveRecord::Schema.define(version: 20141218121504) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20141218121300) do
     t.string   "title"
     t.string   "description"
     t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
