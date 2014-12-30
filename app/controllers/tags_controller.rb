6971cbf50a0628836a8c996c5af388a292058d2e
Lisa M Schachtschneider <islisamarie@gmail.com>
2014-12-30 11:45:31 +0100

    adding images to about me page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,16 @@
 class TagsController < ApplicationController
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    if @tag.save?
+      redirect_to posts_path
+    end
+  end
+  private
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 end
